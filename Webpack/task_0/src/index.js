7ee7d7368e47929562c653c936f1b1cf07550679
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-07 17:43:22 -0500

    added files

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 // import jquery and add three different paragraphs to the page body
 import $ from "jquery";
 
-$("<p>Holberton Dashboard</p>").appendTo($('body'));
-$("<p>Dashboard data for the students</p>").appendTo($('body'));
-$("<p>Copyright - Holberton School</p>").appendTo($('body'));
+$("<p>Holberton Dashboard</p>").appendTo(document.body);
+$("<p>Dashboard data for the students</p>").appendTo(document.body);
+$("<p>Copyright - Holberton School</p>").appendTo(document.body);
