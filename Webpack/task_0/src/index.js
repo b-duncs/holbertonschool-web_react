f5495b21ee7c951c38b8c97d051d1bf4cf4d2826
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-07 17:38:42 -0500

    added files

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+// import jquery and add three different paragraphs to the page body
+import $ from "jquery";
+
+$("<p>Holberton Dashboard</p>").appendTo($('body'));
+$("<p>Dashboard data for the students</p>").appendTo($('body'));
+$("<p>Copyright - Holberton School</p>").appendTo($('body'));
