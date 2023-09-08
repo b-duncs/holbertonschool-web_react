cb9c63940e44aa1e6a83c18e4565452eb20b89e3
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-08 16:18:12 -0500

    added files

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+import { RowElement, RowID } from './interface';
+
+export type insertRow = (row: RowElement) => number;
+
+export type deleteRow = (rowId: RowID) => void;
+
+export type updateRow = (rowId: RowID, row: RowElement) => number;
+
