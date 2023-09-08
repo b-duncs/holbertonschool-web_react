11391e298aa0b4494b9a4e9d336f026c10089d5f
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-07 20:37:48 -0500

    updated files

--- Webpack/npm_server_installs.sh
+++ Webpack/npm_server_installs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npm install http-server --save-dev
-npm install concurrently --save-dev
+#npm install http-server --save-dev
+#npm install concurrently --save-dev
+npm install -D webpack-dev-server
