bce391d5f42df99ef53ebe4bae55648eed98be5b
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-07 20:05:28 -0500

    added files

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -6,5 +6,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre'
+      },
+    ]
+  },
   mode: "production"
 };
