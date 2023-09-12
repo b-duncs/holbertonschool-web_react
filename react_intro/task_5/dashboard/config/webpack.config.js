b91dc11a97ad3b6b6f817c9db37bc3e12bda0e14
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-09-11 23:05:32 -0500

    meaningful commit for holberton staff: updated files for task five

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+    hot: true,
+    compress: true,
+    port: 8564,
+  },
+  entry: {
+    index: './src1/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks:{
+      chunks: 'all',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'image-webpack-loader',
+          'file-loader'
+        ],
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre',
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+};
